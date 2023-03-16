9034ad5028284613cdef4bf96ced779d8f92af4d
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-03-16 15:37:08 +0330

    cats

--- configs/.config/nvim/lua/kazuma/catppuccin.lua
+++ configs/.config/nvim/lua/kazuma/catppuccin.lua
@@ -7,6 +7,7 @@ end
 catppuccin.setup({
 	flavour = "mocha",
 	transparent_background = false,
+    show_end_of_buffer = false,
 	styles = {
 		comments = { "italic" },
 		-- conditionals = { "italic" },
