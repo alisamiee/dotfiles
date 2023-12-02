f59381b6a74161e58bbe88091ae5dd464fa8ca50
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2023-12-02 20:03:04 +0330

    update nvim config

--- configs/.config/nvim/lua/sakura/indentline.lua
+++ configs/.config/nvim/lua/sakura/indentline.lua
@@ -1,27 +1,21 @@
-local present, indent_blankline = pcall(require, "indent_blankline")
+local present, indent_blankline = pcall(require, "ibl")
 if not present then
-	vim.notify("indent_blankline not found.")
-	return
+    vim.notify("indent_blankline not found.")
+    return
 end
 
 indent_blankline.setup({
-	show_trailing_blankline_indent = false,
-	show_first_indent_level = true,
-	use_treesitter = true,
-	show_current_context = true,
-	buftype_exclude = { "terminal", "nofile" },
-	filetype_exclude = {
-		"help",
-		"packer",
-		"NvimTree",
-	},
-	-- space_char_blankline = " ",
-	-- char_highlight_list = {
-	-- 	"IndentBlanklineIndent1",
-	-- 	"IndentBlanklineIndent2",
-	-- 	"IndentBlanklineIndent3",
-	-- 	"IndentBlanklineIndent4",
-	-- 	"IndentBlanklineIndent5",
-	-- 	"IndentBlanklineIndent6",
-	-- },
+    whitespace = {
+        remove_blankline_trail = true,
+    },
+    scope = {
+        enabled = true,
+    },
+    exclude = {
+        buftypes = { "terminal", "nofile" },
+        filetypes = {
+            "help",
+            "packer",
+        },
+    },
 })
