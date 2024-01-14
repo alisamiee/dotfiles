e5d331d812902dc59bcf42070f41ba4d371a03c7
Ali Samiee <99135990+AliSamiee@users.noreply.github.com>
2024-01-14 21:30:44 +0330

    remove old nvim config

--- configs/.config/nvim/lua/kazuma/impatient.lua
+++
@@ -1,7 +0,0 @@
-local present, impatient = pcall(require, "impatient")
-if not present then
-	vim.notify("impatient not found.")
-	return
-end
-
-impatient.enable_profile()
--- configs/.config/nvim/lua/kazuma/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local present, _ = pcall(require, "lspconfig")
-if not present then
-	vim.notify("lspconfig not found.")
-	return
-end
-
-require("kazuma.lsp.mason")
-require("kazuma.lsp.handlers").setup()
-require("kazuma.lsp.null-ls")
